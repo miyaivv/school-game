d6b05b7bf5bf0e0a5fe248df16aedc999e226bea
Idk <lionmgirl@gmail.com>
2026-05-19 12:01:17 +0300

    nz

--- Assets/Scripts/Other/GameManager.cs
+++ Assets/Scripts/Other/GameManager.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame ()
     {
-        
+
+    }
+
+    public void EndGame()
+    {
+
     }
 }
